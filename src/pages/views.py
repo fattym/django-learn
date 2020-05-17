3169f46f3875152f4cb136980083d8a6bf9a2824
fattym <jnganga288@gmail.com>
2020-05-17 13:03:10 +0300

    commited changes

--- src/pages/views.py
+++ src/pages/views.py
@@ -6,7 +6,17 @@ def home_view(request,*args, **kwargs):
     print(args, kwargs)
     print(request.user)
     return render(request, "home.html", {})
+    
+def about_view(request, *args, **kwargs):
+    my_context = {
+        "title": "abc this is about us",
+        "this_is_true": True,
+        "my_number": 123,
+        "my_list": [1313, 4231, 312, "Abc"],
+        "my_html": "<h1>Hello World</h1>"
 
+    }
+    return render(request, "about.html", my_context)
 
 def contact_view(request,*args, **kwargs):
     
